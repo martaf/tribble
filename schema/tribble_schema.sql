cac9146a861d1acaaab0b8f3960746c175888895
Pedro Correia <pedro.a.correia@co.sapo.pt>
2011-12-26 18:19:27 +0000

    Some folder cleanup and includes the newest db schema

--- schema/tribble_schema.sql
+++ schema/tribble_schema.sql
@@ -1,21 +1,17 @@
 --
 -- MySQL 5.5.16
--- Tue, 20 Dec 2011 13:10:56 +0000
+-- Mon, 26 Dec 2011 18:18:47 +0000
 --
 
-CREATE DATABASE `tribble_schema` DEFAULT CHARSET utf8;
+CREATE TABLE `tr_images` (
+   `image_id` int(11) not null auto_increment,
+   `image_tribble_id` int(11) not null,
+   `image_path` varchar(255) not null,
+   `image_palette` varchar(255) not null,
+   PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=5;
 
-USE `tribble_schema`;
-
-CREATE TABLE `tr_comments` (
-   `comment_id` int(11) not null,
-   `comment_text` varchar(255) not null,
-   `comment_image` varchar(255),
-   `comment_user_id` int(11) not null,
-   PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- [Table `tr_comments` is empty]
+-- [Table `tr_images` is empty]
 
 CREATE TABLE `tr_likes` (
    `like_id` int(11) not null,
@@ -26,12 +22,48 @@ CREATE TABLE `tr_likes` (
 
 -- [Table `tr_likes` is empty]
 
-CREATE TABLE `tr_posts` (
-   `post_id` int(11) not null auto_increment,
-   `post_text` varchar(255) not null,
-   `post_image` varchar(255) not null,
-   `post_user_id` int(11) not null,
-   PRIMARY KEY (`post_id`)
+CREATE TABLE `tr_sessions` (
+   `session_id` varchar(40) not null default '0',
+   `ip_address` varchar(16) not null default '0',
+   `user_agent` varchar(120) not null,
+   `last_activity` int(10) unsigned not null default '0',
+   `user_data` text not null,
+   PRIMARY KEY (`session_id`),
+   KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- [Table `tr_sessions` is empty]
+
+CREATE TABLE `tr_tags` (
+   `tags_id` int(11) not null auto_increment,
+   `tags_tribble_id` int(11),
+   `tags_content` varchar(255) not null,
+   PRIMARY KEY (`tags_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
--- [Table `tr_posts` is empty]
+-- [Table `tr_tags` is empty]
+
+CREATE TABLE `tr_tribbles` (
+   `tribble_id` int(11) not null auto_increment,
+   `tribble_parent` int(11) not null default '0',
+   `tribble_title` varchar(255) not null,
+   `tribble_text` varchar(255) not null,
+   `tribble_user_id` int(11) not null,
+   `tribble_views` int(11) not null default '0',
+   `tribble_timestamp` timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+   PRIMARY KEY (`tribble_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=8;
+
+-- [Table `tr_tribbles` is empty]
+
+CREATE TABLE `tr_users` (
+   `user_id` int(11) not null auto_increment,
+   `user_email` varchar(255) not null,
+   `user_password` varchar(128) not null,
+   `user_realname` varchar(255) not null,
+   `user_bio` text,
+   PRIMARY KEY (`user_id`),
+   UNIQUE KEY (`user_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=3;
+
+-- [Table `tr_users` is empty]
